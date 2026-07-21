1e1b038e98ab01051561fc504b29ce1310c551b5
Mclean Oshiokpekhai <mcleanoshio@outlook.com>
2026-07-20 22:52:46 -0500

    making some intial comments

--- src/services/optimizer.ts
+++ src/services/optimizer.ts
@@ -14,12 +14,21 @@ export type DeckCard = {
   name: string;
   data: ScryfallCard;
 };
+/*
+So, here's a potential formula we could use:
+from: https://www.tcgplayer.com/content/article/How-Many-Lands-Do-You-Need-in-Your-Deck-An-Updated-Analysis/
+"= 99/60 * (19.59 + 1.90 * average mana value + 0.27) – 0.28 * number of cheap card draw or mana ramp spells - 1.35 
 
+= 31.42 + 3.13 * average mana value of your spells – 0.28 * number of cheap card draw or mana ramp spells"
+
+Getting Average Mana value from a deck shouldn't be too hard, 
+what would be harder is trying to figure out what's cheap draw/mana ramp
+
+*/
 export function optimizeMana(deck: DeckCard[]): ManaBreakdown {
   const colorIdentity = getDeckColorIdentity(deck);
 
   //TODO: factor in color pips
-
   const landCount = countCards(deck, isLand);
   const nonLandCount = countCards(deck, (card) => !isLand(card));
 
@@ -40,7 +49,13 @@ function getDeckColorIdentity(deck: DeckCard[]): ManaColor[] {
   });
 
   // Convert Set to sorted array for standard formatting (WUBRG)
-  const colorOrder: Record<ManaColor, number> = { W: 1, U: 2, B: 3, R: 4, G: 5 };
+  const colorOrder: Record<ManaColor, number> = {
+    W: 1,
+    U: 2,
+    B: 3,
+    R: 4,
+    G: 5,
+  };
   return Array.from(identity).sort((a, b) => colorOrder[a] - colorOrder[b]);
 }
 
@@ -52,5 +67,7 @@ function countCards(
   deck: DeckCard[],
   predicate: (card: DeckCard) => boolean,
 ): number {
-  return deck.filter(predicate).reduce((total, card) => total + card.quantity, 0);
+  return deck
+    .filter(predicate)
+    .reduce((total, card) => total + card.quantity, 0);
 }
